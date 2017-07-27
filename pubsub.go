81441c333ed234e7ba023749d6b72918989a09fa
Karl Mutch <karlmutch@gmail.com>
2017-07-27 15:55:29 -0700

    Test sub package

--- pubsub.go
+++ pubsub.go
@@ -4,10 +4,10 @@ package runner
 // authentication used by google
 
 import (
+	"context"
 	"fmt"
 	"os"
 
-	"cloud.google.com/go"
 	"cloud.google.com/go/pubsub"
 	"cloud.google.com/go/storage"
 	"google.golang.org/api/option"
@@ -26,7 +26,10 @@ func init() {
 func getCred() (opts option.ClientOption, err error) {
 	val, isPresent := os.LookupEnv("GOOGLE_APPLICATION_CREDENTIALS")
 	if !isPresent {
-		return nil, fmt.Errorf("the environment variable GOOGLE_APPLICATION_CREDENTIALS was not set")
+		return nil, fmt.Errorf(`the environment variable GOOGLE_APPLICATION_CREDENTIALS was not set,
+		fix this by creating a service account key using your Web based GCP console and then save the 
+		resulting file into a safe location and define an environment variable 
+		GOOGLE_APPLICATION_CREDENTIALS to point at this file`)
 	}
 
 	return option.WithServiceAccountFile(val), nil
@@ -36,7 +39,7 @@ type PubSub struct {
 	Client *pubsub.Client
 }
 
-func NewPubSub(qName string) (pubsub PubSub, err error) {
+func NewPubSub(qName string) (pubsub *PubSub, err error) {
 	cred, err := getCred()
 	if err != nil {
 		return nil, err
@@ -46,5 +49,5 @@ func NewPubSub(qName string) (pubsub PubSub, err error) {
 	if pubsub.Client, err = pubsub.NewClient(ctx, qName, cred); err != nil {
 		return nil, err
 	}
-	return pubsub
+	return &pubsub, nil
 }
