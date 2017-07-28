1b28accb80f236bd5ab4fe4035e234a036ea1861
Karl Mutch <karlmutch@gmail.com>
2017-07-28 13:31:43 -0700

    Consumer now looped in with JSON parser for TF payloads, now needs respones

--- cmd/runner/main.go
+++ cmd/runner/main.go
@@ -1,66 +1,116 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
+	"os"
+	"os/signal"
+	"path"
+	"syscall"
+	"time"
 
 	"github.com/karlmutch/studio-go-runner"
 
-	"cloud.google.com/go/pubsub"
-	"golang.org/x/net/context"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-
+	"github.com/karlmutch/envflag"
 	"github.com/mgutz/logxi/v1"
 )
 
 var (
 	logger log.Logger
+
+	projectOpt = flag.String("tf-project", "tfstudio-a8367", "the google project id")
+	queueOpt   = flag.String("tf-queue", "TFStudioTest", "the google project id")
 )
 
 func init() {
 	logger = log.New("runner")
 }
 
+func usage() {
+	fmt.Fprintln(os.Stderr, path.Base(os.Args[0]))
+	fmt.Fprintln(os.Stderr, "usage: ", os.Args[0], "[arguments]      Run the TFStudio, DarkCycle® gateway")
+	fmt.Fprintln(os.Stderr, "")
+	fmt.Fprintln(os.Stderr, "Arguments:")
+	flag.PrintDefaults()
+	fmt.Fprintln(os.Stderr, "")
+	fmt.Fprintln(os.Stderr, "Environment Variables:")
+	fmt.Fprintln(os.Stderr, "")
+	fmt.Fprintln(os.Stderr, "The GOOGLE_APPLICATION_CREDENTIALS env variable needs to be set before running this software.")
+	fmt.Fprintln(os.Stderr, "More information can be found at https://developers.google.com/identity/protocols/application-default-credentials.")
+	fmt.Fprintln(os.Stderr, "These credentials are used to access resources used by the TFStudio client to")
+	fmt.Fprintln(os.Stderr, "retrieve compute requests from users.")
+	fmt.Fprintln(os.Stderr, "")
+	fmt.Fprintln(os.Stderr, "To control log levels the LOGXI env variables can be used, these are documented at https://github.com/mgutz/logxi")
+}
+
 func main() {
 
-	logger.SetLevel(log.LevelInfo)
+	flag.Usage = usage
 
-	projectID := "tfstudio-a8367"
-	qName := "TFStudioTest"
+	// Use the go options parser to load command line options that have been set, and look
+	// for these options inside the env variable table
+	//
+	envflag.Parse()
 
-	ps, err := runner.NewPubSub(projectID)
+	// Supplying the context allows the client to pubsub to cancel the
+	// blocking receive inside the run
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// Setup a channel to allow a CTRL-C to terminate all processing.  When the CTRL-C
+	// occurs we cancel the background msg pump processing pubsub mesages from
+	// google, and this will also cause the main thread to unblock and return
+	//
+	stopC := make(chan os.Signal)
+	go func() {
+		defer cancel()
+
+		select {
+		case <-stopC:
+			log.Warn("CTRL-C Seen")
+			return
+		}
+	}()
+
+	signal.Notify(stopC, os.Interrupt, syscall.SIGTERM)
+
+	newCtx, newCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ps, err := runner.NewPubSub(newCtx, *projectOpt, *queueOpt, *queueOpt+"_sub")
 	if err != nil {
 		logger.Fatal(fmt.Sprintf("could not start the pubsub listener due to %v", err))
 	}
-	defer ps.Client.Close()
+	newCancel()
 
-	ctx := context.Background()
-	topic, err := ps.Client.CreateTopic(ctx, projectID)
-	if err != nil {
-		if grpc.Code(err) != codes.AlreadyExists {
-			logger.Fatal(fmt.Sprintf("could not create the topic for pubsub listener due to %v", err))
-		}
-	}
-	defer topic.Stop()
+	defer ps.Close()
 
-	sub, err := ps.Client.CreateSubscription(ctx, qName+"_sub", pubsub.SubscriptionConfig{Topic: topic})
-	if err != nil {
-		if grpc.Code(err) != codes.AlreadyExists {
-			logger.Fatal(fmt.Sprintf("could not create the subscription for our listener due to %#v", err))
-		}
-	}
-
-	for {
-		err := sub.Receive(ctx,
-			func(ctx context.Context, msg *pubsub.Message) {
-				logger.Debug(fmt.Sprintf("%#v", *msg))
-				logger.Info(fmt.Sprintf("%s", string(msg.Data)))
-
+	// Start an asynchronous function that will listen for messages from pubsub,
+	// errors also being sent related to pubsub failures that might occur,
+	// and also for cancellations being signalled via the processing context
+	// this server will use
+	//
+	go func() {
+		defer close(stopC)
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-ps.ErrorC:
+				logger.Fatal(fmt.Sprintf("TFStudio message receiver stopped due to %s", err))
+			case msg := <-ps.MsgC:
+				rqst, err := runner.UnmarshalRequest(msg.Data)
+				if err != nil {
+					logger.Warn(fmt.Sprintf("could not unmarshal a msg from TFStudio due to %v", err))
+					continue
+				}
+				logger.Info(fmt.Sprintf("%#v", rqst))
 				msg.Ack()
-			})
-		if err != nil {
-			logger.Fatal(fmt.Sprintf("subscription receiver failed due to %v", err))
+			}
 		}
+	}()
+
+	if err = ps.Start(ctx); err != nil {
+		logger.Fatal(fmt.Sprintf("could not continue listening for msgs due to %v", err))
 	}
 
+	<-stopC
 }
--- pubsub.go
+++ pubsub.go
@@ -4,13 +4,18 @@ package runner
 // authentication used by google
 
 import (
-	"context"
 	"fmt"
 	"os"
+	"sync"
+	"time"
 
 	"cloud.google.com/go/pubsub"
 	"cloud.google.com/go/storage"
 	"google.golang.org/api/option"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+
+	"golang.org/x/net/context"
 )
 
 var (
@@ -32,22 +37,119 @@ func getCred() (opts option.ClientOption, err error) {
 	return option.WithServiceAccountFile(val), nil
 }
 
+// PubSub encapsulate a Google PubSub msg queue using channels as orchestration
+//
 type PubSub struct {
-	Client *pubsub.Client
+	client *pubsub.Client
+	topic  *pubsub.Topic
+	sub    *pubsub.Subscription
+	MsgC   chan *pubsub.Message
+	ErrorC chan error
+	sync.Mutex
 }
 
-func NewPubSub(projectID string) (ps *PubSub, err error) {
+// NewPubSub is used to initialize a new PubSub subscriber and listener
+//
+func NewPubSub(ctx context.Context, projectID string, topicID string, subscriptionID string) (ps *PubSub, err error) {
 
-	ps = &PubSub{}
+	ps = &PubSub{
+		// No buffering on the msg channel so that Nacks can be done based upon
+		// an internal timeout back to the pubsub Q
+		MsgC: make(chan *pubsub.Message, 0),
+		// ErrorC is used by asynchronous go functions to post any errors they encounter
+		ErrorC: make(chan error, 1),
+	}
 
 	cred, err := getCred()
 	if err != nil {
 		return nil, err
 	}
 
-	ctx := context.Background()
-	if ps.Client, err = pubsub.NewClient(ctx, projectID, cred); err != nil {
+	if ps.client, err = pubsub.NewClient(ctx, projectID, cred); err != nil {
 		return nil, err
 	}
+
+	ps.topic, err = ps.client.CreateTopic(ctx, projectID)
+	if err != nil {
+		if grpc.Code(err) != codes.AlreadyExists {
+			return nil, err
+		}
+	}
+
+	ps.sub, err = ps.client.CreateSubscription(ctx, subscriptionID, pubsub.SubscriptionConfig{Topic: ps.topic})
+	if err != nil {
+		if grpc.Code(err) != codes.AlreadyExists {
+			return nil, err
+		}
+	}
 	return ps, nil
+}
+
+// Close is used to release any resources associated with the google pubsub listeners, including pending
+// operations that await completion
+//
+func (ps *PubSub) Close() (err error) {
+	if ps == nil {
+		return fmt.Errorf("nil pubsub supplied")
+	}
+
+	ps.Lock()
+	defer ps.Unlock()
+
+	if ps.client == nil {
+		return nil
+	}
+
+	ps.topic.Stop()
+
+	ps.client.Close()
+	ps.client = nil
+
+	close(ps.MsgC)
+	close(ps.ErrorC)
+
+	return nil
+}
+
+// run is a blocking function that accepts messages from pubsub and pushes them into
+// a go channel for processing, clients should ack messages explicitly otherwise
+// they will be resent by pubsub to other subscribers.
+//
+func (ps *PubSub) run(ctx context.Context) (err error) {
+	for {
+		err = ps.sub.Receive(ctx,
+			func(ctx context.Context, msg *pubsub.Message) {
+				select {
+				case ps.MsgC <- msg:
+					msg.Ack()
+				case <-time.After(time.Second):
+					msg.Nack()
+				}
+			})
+		if err != nil {
+			select {
+			case <-ctx.Done():
+				return
+			case ps.ErrorC <- err:
+				continue
+			case <-time.After(time.Second):
+				// The error has no listeners, we have nothing that
+				// can be done after this point other than return
+				return err
+			}
+		}
+	}
+}
+
+func (ps *PubSub) Start(ctx context.Context) (err error) {
+
+	if ps.client == nil {
+		return fmt.Errorf("pubsub not activated, or has been closed")
+	}
+
+	go func() {
+		ps.run(ctx)
+	}()
+
+	return nil
 }
