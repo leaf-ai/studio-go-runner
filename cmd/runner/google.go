299bfebd78b08a809c7adfdaa7e4a158f2e528cf
Karl Mutch <karlmutch@gmail.com>
2017-11-03 15:09:22 -0700

    Added skeleton for the google credentials refresher

---
+++ cmd/runner/google.go
@@ -0,0 +1,8 @@
+package main
+
+// The file contains code for handling google certificates and
+// refreshing a directory containing these certificates and using
+// these to process work sent to pubsub queues that get forwarded
+// to subscriptions made by the runner
+
+import ()
