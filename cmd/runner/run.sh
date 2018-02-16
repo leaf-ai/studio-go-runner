23b6df916afbd8277e4ef1ddcb2954b7c31f8f76
Karl Mutch <karlmutch@gmail.com>
2018-02-16 06:16:42 -0800

    acs-engine working save for others to refer to

--- cmd/runner/run.sh
+++ cmd/runner/run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -e
+
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/cuda/lib64
+ls /usr/local/cuda
+ls /usr/local/cuda/lib64
+nvidia-smi
 SLACK_HOOK="https://hooks.slack.com/services/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" SLACK_ROOM="#studioml-ops" LOGXI_FORMAT=happy,maxcol=1024 LOGXI=*=TRC  ./runner -debug -sqs-certs certs -google-certs certs
 
