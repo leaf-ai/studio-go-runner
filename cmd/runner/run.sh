a031669c0d98a51ceeb0512aac2d14b11052cddb
Karl Mutch <karlmutch@gmail.com>
2018-02-17 16:44:26 -0800

    fix naming issues with files and directories used for google credentials

--- cmd/runner/run.sh
+++ cmd/runner/run.sh
@@ -6,6 +6,8 @@ ls /usr/local/cuda
 echo "**/usr/local/cuda/lib64"
 ls /usr/local/cuda/lib64
 find / -name libnvidia-ml.so
+echo "certs"
+ls certs
 nvidia-smi
 SLACK_HOOK="https://hooks.slack.com/services/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" SLACK_ROOM="#studioml-ops" LOGXI_FORMAT=happy,maxcol=1024 LOGXI=*=TRC  ./runner -debug -sqs-certs certs -google-certs certs
 
