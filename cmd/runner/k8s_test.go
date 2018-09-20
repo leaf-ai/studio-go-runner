0a44622aacad678a688a842c2a76d23bb5257683
Karl Mutch <karlmutch@gmail.com>
2018-09-19 17:42:52 -0700

    Additional logging for test validation

--- cmd/runner/k8s_test.go
+++ cmd/runner/k8s_test.go
@@ -123,6 +123,7 @@ func TestK8sConfigNode(t *testing.T) {
 			case <-ctx.Done():
 				return
 			case update := <-stateC:
+				logger.Info(update)
 				state = update.State
 				// Wakeup the test waiting on state changes, if a message is
 				// already queued up then abandon the send
