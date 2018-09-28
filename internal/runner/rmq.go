e8b08f4f0db783c09abeac3bf601679edcc24ca8
Karl Mutch <karlmutch@gmail.com>
2018-09-28 14:15:01 -0700

    Relocate code the cmd test relies on being in the non test code from the internal packages

--- internal/runner/rmq.go
+++ internal/runner/rmq.go
@@ -333,3 +333,57 @@ func PingRMQServer(amqpURL string) (err errors.Error) {
 
 	return testQErr
 }
+
+// QueueDeclare is a shim method for creating a queue within the rabbitMQ
+// server defined by the receiver
+//
+func (rmq *RabbitMQ) QueueDeclare(qName string) (err errors.Error) {
+	conn, ch, err := rmq.attachQ()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		ch.Close()
+		conn.Close()
+	}()
+
+	_, errGo := ch.QueueDeclare(
+		qName, // name
+		false, // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	if errGo != nil {
+		return errors.Wrap(errGo).With("stack", stack.Trace().TrimRuntime()).With("qName", qName).With("uri", rmq.mgmt).With("exchange", rmq.exchange)
+	}
+	return nil
+}
+
+// Publish is a shim method for tests to use for sending requeues to a queue
+//
+func (rmq *RabbitMQ) Publish(routingKey string, contentType string, msg []byte) (err errors.Error) {
+	conn, ch, err := rmq.attachQ()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		ch.Close()
+		conn.Close()
+	}()
+
+	errGo := ch.Publish(
+		rmq.exchange, // exchange
+		routingKey,   // routing key
+		false,        // mandatory
+		false,        // immediate
+		amqp.Publishing{
+			ContentType: contentType,
+			Body:        msg,
+		})
+	if errGo != nil {
+		return errors.Wrap(errGo).With("stack", stack.Trace().TrimRuntime()).With("routingKey", routingKey).With("uri", rmq.mgmt).With("exchange", rmq.exchange)
+	}
+	return nil
+}
