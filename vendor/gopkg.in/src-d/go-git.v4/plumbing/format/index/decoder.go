c2546bcc85520c202debe0bfef9b579a09578119
Karl Mutch <karlmutch@gmail.com>
2018-10-23 07:09:13 -0700

    Minimal licenses extractor, needs better packaging

--- vendor/gopkg.in/src-d/go-git.v4/plumbing/format/index/decoder.go
+++ vendor/gopkg.in/src-d/go-git.v4/plumbing/format/index/decoder.go
@@ -21,7 +21,7 @@ var (
 	// ErrMalformedSignature is returned by Decode when the index header file is
 	// malformed
 	ErrMalformedSignature = errors.New("malformed index signature file")
-	// ErrInvalidChecksum is returned by Decode if the SHA1 hash mismatch with
+	// ErrInvalidChecksum is returned by Decode if the SHA1 hash missmatch with
 	// the read content
 	ErrInvalidChecksum = errors.New("invalid checksum")
 
