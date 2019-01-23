48127513c95cd25fe14c79b6d1f22de9ea796509
Karl Mutch <karlmutch@gmail.com>
2019-01-23 11:00:36 -0800

    Commit base for working on the forked repository

--- build.sh
+++ build.sh
@@ -76,15 +76,17 @@ export GIT_BRANCH=`echo '{{.duat.gitBranch}}'|stencil - | tr '_' '-' | tr '\/' '
 export RUNNER_BUILD_LOG=build-$GIT_BRANCH.log
 exit_code=0
 
+# Build the base image that other images will derive from for development style images
+docker build -t leafai/studio-go-runner-dev-base:$GIT_BRANCH -f Dockerfile_base .
+
 travis_fold start "build.image"
     travis_time_start
-        stencil -input Dockerfile | docker build -t leaf-ai/studio-go-runner/build:$GIT_BRANCH -
+        stencil -input Dockerfile | docker build -t leafai/studio-go-runner-build:$GIT_BRANCH -
         exit_code=$?
         if [ $exit_code -ne 0 ]; then
             exit $exit_code
         fi
-        docker tag leaf-ai/studio-go-runner/build:$GIT_BRANCH leaf-ai/studio-go-runner/build:latest
-        stencil -input Dockerfile_full | docker build -t leaf-ai/studio-go-runner/standalone-build:$GIT_BRANCH -
+        stencil -input Dockerfile_full | docker build -t leafai/studio-go-runner-standalone-build:$GIT_BRANCH -
         exit_code=$?
         if [ $exit_code -ne 0 ]; then
             exit $exit_code
@@ -99,7 +101,7 @@ fi
 # Running build.go inside of a container will result in a compilation, light testing, and release however no docker images
 travis_fold start "build"
     travis_time_start
-        docker run -e TERM="$TERM" -e LOGXI="$LOGXI" -e LOGXI_FORMAT="$LOGXI_FORMAT" -e GITHUB_TOKEN=$GITHUB_TOKEN -v $GOPATH:/project leaf-ai/studio-go-runner/build:$GIT_BRANCH
+        docker run -e TERM="$TERM" -e LOGXI="$LOGXI" -e LOGXI_FORMAT="$LOGXI_FORMAT" -e GITHUB_TOKEN=$GITHUB_TOKEN -v $GOPATH:/project leafai/studio-go-runner-build:$GIT_BRANCH
         exit_code=$?
         if [ $exit_code -ne 0 ]; then
             exit $exit_code
@@ -119,8 +121,7 @@ travis_fold start "image.build"
         if [ $exit_code -ne 0 ]; then
             exit $exit_code
         fi
-        docker build -t leaf-ai/studio-go-runner/build_k8s_local -f Dockerfile_k8s_local .
-        docker tag leaf-ai/studio-go-runner/build_k8s_local localhost:32000/leaf-ai/studio-go-runner/build_k8s_local
+        docker build -t studio-go-runner-build_k8s_local:$GIT_BRANCH -f Dockerfile_k8s_local .
     travis_time_finish
 travis_fold end "image.build"
 
@@ -131,42 +132,44 @@ fi
 travis_fold start "image.push"
     travis_time_start
 		if docker image inspect leaf-ai/studio-go-runner/runner:$SEMVER 2>/dev/null 1>/dev/null; then
+			if type docker 2>/dev/null ; then
+                docker login docker.io
+				if [ $? -eq 0 ]; then
+                    docker tag leaf-ai/studio-go-runner/runner:$SEMVER leafai/studio-go-runner:$SEMVER
+                    docker tag leafai/studio-go-runner-build:$GIT_BRANCH leafai/studio-go-runner-build:latest
+
+                    docker push leafai/studio-go-runner-dev-base:$GIT_BRANCH
+					docker push leafai/studio-go-runner:$SEMVER
+                    docker push leafai/studio-go-runner-dev-base:0.0.0
+                    docker push leafai/studio-go-runner-standalone-build:$GIT_BRANCH
+			    fi
+			fi
 			if type aws 2>/dev/null ; then
 				`aws ecr get-login --no-include-email`
 				if [ $? -eq 0 ]; then
 					account=`aws sts get-caller-identity --output text --query Account`
 					if [ $? -eq 0 ]; then
-						docker tag leaf-ai/studio-go-runner/runner:$SEMVER $account.dkr.ecr.us-west-2.amazonaws.com/leaf-ai/studio-go-runner/runner:$SEMVER
-						docker push $account.dkr.ecr.us-west-2.amazonaws.com/leaf-ai/studio-go-runner/runner:$SEMVER
+						docker tag leafai/studio-go-runner:$SEMVER $account.dkr.ecr.us-west-2.amazonaws.com/leafai/studio-go-runner:$SEMVER
+						docker push $account.dkr.ecr.us-west-2.amazonaws.com/leafai/studio-go-runner:$SEMVER
 
-						docker tag leaf-ai/studio-go-runner/standalone-build:$GIT_BRANCH $account.dkr.ecr.us-west-2.amazonaws.com/leaf-ai/studio-go-runner/standalone-build:$GIT_BRANCH
-						docker push $account.dkr.ecr.us-west-2.amazonaws.com/leaf-ai/studio-go-runner/standalone-build:$GIT_BRANCH
+						docker tag leafai/studio-go-runner-standalone-build:$GIT_BRANCH $account.dkr.ecr.us-west-2.amazonaws.com/leafai/studio-go-runner-standalone-build:$GIT_BRANCH
+						docker push $account.dkr.ecr.us-west-2.amazonaws.com/leafai/studio-go-runner-standalone-build:$GIT_BRANCH
 					fi
 				fi
-			fi
-			if type docker 2>/dev/null ; then
-                docker login
-				if [ $? -eq 0 ]; then
-					docker tag leaf-ai/studio-go-runner/runner:$SEMVER karlmutch/studio-go-runner:$SEMVER
-					docker push karlmutch/studio-go-runner:$SEMVER
-
-					docker tag leaf-ai/studio-go-runner/standalone-build:$GIT_BRANCH karlmutch/studio-go-runner-standalone-build:$GIT_BRANCH
-                    docker push karlmutch/studio-go-runner-standalone-build:$GIT_BRANCH
-			    fi
 			fi
 			if type az 2>/dev/null; then
 				if [ -z ${azure_registry_name+x} ]; then
 					:
 				else
 					if az acr login --name $azure_registry_name; then
-						docker tag leaf-ai/studio-go-runner/standalone-build:$GIT_BRANCH $azure_registry_name.azurecr.io/sentient.ai/studio-go-runner/standalone-build:$GIT_BRANCH
-						docker push $azure_registry_name.azurecr.io/sentient.ai/studio-go-runner/standalone-build:$GIT_BRANCH
+						docker tag leafai/studio-go-runner-standalone-build:$GIT_BRANCH $azure_registry_name.azurecr.io/sentient.ai/studio-go-runner-standalone-build:$GIT_BRANCH
+						docker push $azure_registry_name.azurecr.io/sentient.ai/studio-go-runner-standalone-build:$GIT_BRANCH
 
-						docker tag leaf-ai/studio-go-runner/build:$GIT_BRANCH $azure_registry_name.azurecr.io/sentient.ai/studio-go-runner/build:$GIT_BRANCH
-						docker push $azure_registry_name.azurecr.io/sentient.ai/studio-go-runner/build:$GIT_BRANCH
+						docker tag leafai/studio-go-runner-build:$GIT_BRANCH $azure_registry_name.azurecr.io/sentient.ai/studio-go-runner-build:$GIT_BRANCH
+						docker push $azure_registry_name.azurecr.io/sentient.ai/studio-go-runner-build:$GIT_BRANCH
 
-						docker tag leaf-ai/studio-go-runner/runner:$SEMVER $azure_registry_name.azurecr.io/sentient.ai/studio-go-runner/runner:$SEMVER
-						docker push $azure_registry_name.azurecr.io/sentient.ai/studio-go-runner/runner:$SEMVER
+						docker tag leafai/studio-go-runner:$SEMVER $azure_registry_name.azurecr.io/sentient.ai/studio-go-runner:$SEMVER
+						docker push $azure_registry_name.azurecr.io/sentient.ai/studio-go-runner:$SEMVER
 					fi
 				fi
 			fi
