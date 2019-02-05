3ef13d5aab819f8ed68e8b54980f023e20c389be
Karl Mutch <karlmutch@gmail.com>
2019-02-05 09:24:29 -0800

    Dont continue to pump out meaningless messages after quiesing

--- ci.sh
+++ ci.sh
@@ -102,13 +102,15 @@ travis_fold start "build.image"
     travis_time_finish
 travis_fold end "build.image"
 
+cleanup
+
 echo "Starting the namespace injections etc" $K8S_POD_NAME
 kubectl label deployment build keel.sh/policy=force --namespace=$K8S_NAMESPACE
 kubectl scale --namespace $K8S_NAMESPACE --replicas=0 rc/rabbitmq-controller
 kubectl scale --namespace $K8S_NAMESPACE --replicas=0 deployment/minio-deployment
 for (( ; ; ))
 do
-    sleep 10
+    sleep 600
 done
 
 if [ $exit_code -ne 0 ]; then
