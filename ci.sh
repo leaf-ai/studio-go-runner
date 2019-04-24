29932f9909231a7d22f371d67781b93fcaa19246
Karl Mutch <karlmutch@gmail.com>
2019-04-24 14:12:35 -0700

    test build dispatch

--- ci.sh
+++ ci.sh
@@ -98,7 +98,7 @@ export
 
 travis_fold start "build.image"
     travis_time_start
-        set -o pipefail ; (go run build.go -r -dirs=internal && go run build.go -r -dirs=cmd ; exit_code=$?) 2>&1 | tee $RUNNER_BUILD_LOG
+        set -o pipefail ; (go run build.go -r -dirs=internal && go run build.go -r -dirs=cmd ; exit_code=$? ; echo $exit_code) 2>&1 | tee $RUNNER_BUILD_LOG
         [[ exit_code == 0 ]] && echo "Success" || echo "Failure " $exit_code
     travis_time_finish
 travis_fold end "build.image"
