d57bd62c86232f76e4a2738f2a282e52a7628c89
Karl Mutch <karlmutch@gmail.com>
2019-04-25 09:22:44 -0700

    Add second step for releasing a new image to dokerhub on a CI based build

--- ci.sh
+++ ci.sh
@@ -99,7 +99,9 @@ export
 travis_fold start "build.image"
     travis_time_start
         set -o pipefail ; (go run build.go -r -dirs=internal && go run build.go -r -dirs=cmd ; exit_code=$? ; echo $exit_code) 2>&1 | tee $RUNNER_BUILD_LOG
-        [[ exit_code == 0 ]] && echo "Success" || echo "Failure " $exit_code
+        if [ $exit_code -ne 0 ] ; then
+            echo "Failure " $exit_code
+        fi
     travis_time_finish
 travis_fold end "build.image"
 
@@ -126,7 +128,7 @@ if [ $exit_code -eq 0 ]; then
     echo "imagebuild-mounted starting" $K8S_POD_NAME
 # Run the docker image build using Mikasu within the same namespace we are occupying and
 # the context for the image build will be the /build mount
-    stencil -values Namespace=$K8S_NAMESPACE -input ci_containerize.yaml | kubectl --namespace $K8S_NAMESPACE create -f -
+    stencil -values Namespace=$K8S_NAMESPACE -input ci_release_image_microk8s.yaml | kubectl --namespace $K8S_NAMESPACE create -f -
     until kubectl --namespace $K8S_NAMESPACE  get job/imagebuilder -o jsonpath='{.status.conditions[].status}' | grep True ; do sleep 3 ; done
     echo "imagebuild-mounted complete" $K8S_POD_NAME
     kubectl --namespace $K8S_NAMESPACE logs job/imagebuilder
