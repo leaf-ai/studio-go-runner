697edac595530edfeea5f5e08b8843cc0073b78c
Karl Mutch <karlmutch@gmail.com>
2019-10-24 14:51:02 -0700

    Add comments

--- cloud/install.sh
+++ cloud/install.sh
@@ -7,6 +7,11 @@ wget -O installer/rabbitmq/user-data https://raw.githubusercontent.com/leaf-ai/s
 echo "n" | ssh-keygen -t ed25519 -N "" -f ~/.ssh/id_ed25519
 export PUBLIC_KEY=`cat ~/.ssh/id_ed25519.pub`
 storage_account=`echo -e "bootlogs$(uuidgen | md5sum - | cut -c1-8)"`
+#
+# Change the following three parameters to suit your specific situation.  Choosing the correct LOCATION
+# is critical for preventing excessive data transfers costs and should be the same as where you intend
+# on deploying your GPU AI infrastructure
+#
 export LOCATION=eastus
 export MINIO_ACCESS_KEY=[An access key you choose and is secret to you, and users of StudioML, LEAF]
 export MINIO_SECRET_KEY=[A secret key you choose and is secret to you, and users of StudioML, LEAF.  Must be at least 8 characters in length.]
@@ -17,6 +22,10 @@ az storage account create --resource-group minio --name $storage_account  --sku 
 az vm create --name minio --resource-group minio --location $LOCATION --data-disk-sizes-gb 10 --boot-diagnostics-storage $storage_account --authentication-type ssh --generate-ssh-keys --image Canonical:UbuntuServer:18.04-LTS:latest --public-ip-address-allocation static --size Standard_D4s_v3 --custom-data minio-user-data-$storage_account -o none
 export MINIO_ADDRESS=$(az network public-ip list --resource-group minio --query "[].ipAddress" --output tsv)
 az vm open-port --port 9000 --resource-group minio --name minio -o none
+#
+# Set passwords that are private to your installation in the next two lines.  Choose passwords that 
+# are more than 8 characters.
+#
 export RMQ_ADMIN_PASSWORD=[A secret key you choose and is secret to the administrator]
 export RMQ_USER_PASSWORD=[A secret key you choose and is secret to users of StudioML, or LEAF]
 envsubst < installer/rabbitmq/user-data > rabbitmq-user-data-$storage_account
