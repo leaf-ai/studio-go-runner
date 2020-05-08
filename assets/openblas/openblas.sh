a7809e17677a26c6b81f252b6ace785c9c043ed2
Karl Mutch <karlmutch@gmail.com>
2020-05-07 17:25:30 -0700

    openblas installed by default, several crypto libraries updated, unit test added,

---
+++ assets/openblas/openblas.sh
@@ -0,0 +1,4 @@
+virtualenv venv -p python3
+source venv/bin/activate
+pip3 install numpy
+python3 openblas.py
--- internal/runner/block_crypto_test.go
+++ internal/runner/block_crypto_test.go
@@ -4,14 +4,12 @@ package runner
 
 import (
 	"context"
-	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
 	"testing"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/go-stack/stack"
 	"github.com/go-test/deep"
 	"github.com/jjeffery/kv"
@@ -80,59 +78,13 @@ func TestCryptoPython(t *testing.T) {
 		filepath.Join("..", "..", "assets", "crypto", "encryptor.py"): 0600,
 		filepath.Join("..", "..", "assets", "crypto", "encryptor.sh"): 0700,
 	}
-
-	expectedScript := ""
-
-	for fn, mode := range testFiles {
-		assetFN, errGo := filepath.Abs(fn)
-		if errGo != nil {
-			t.Fatal(kv.Wrap(errGo).With("stack", stack.Trace().TrimRuntime()))
-		}
-
-		destFN, errGo := filepath.Abs(filepath.Join(tmpDir, filepath.Base(assetFN)))
-		if errGo != nil {
-			t.Fatal(kv.Wrap(errGo).With("stack", stack.Trace().TrimRuntime()))
-		}
-
-		if _, err := CopyFile(assetFN, destFN); err != nil {
-			t.Fatal(err)
-		}
-
-		if errGo = os.Chmod(destFN, mode); errGo != nil {
-			t.Fatal(kv.Wrap(errGo).With("destFN", destFN, "stack", stack.Trace().TrimRuntime()))
-		}
-
-		if mode == 0700 {
-			expectedScript = destFN
-		}
-	}
-
-	// Save the output from the run using the last say 10 lines
-	lineStack := make([]string, 10, 10)
-
-	// Now setup is done execute the experiment
-	dataC := make(chan *string, 1)
-	go func() {
-		for {
-			select {
-			case line := <-dataC:
-				if line == nil {
-					return
-				}
-				// Push to the back of the stack of lines, then pop from the front
-				lineStack = append(lineStack, *line)
-				lineStack = lineStack[1:]
-				fmt.Println(*line)
-			}
-		}
-	}()
-
-	if err := CmdRun(context.TODO(), expectedScript, dataC); err != nil {
-		fmt.Println(spew.Sdump(lineStack))
+	output, err := runPythonTest(testFiles, tmpDir, 3)
+	if err != nil {
 		t.Fatal(err)
 	}
-	clear := lineStack[len(lineStack)-2]
-	encrypted := lineStack[len(lineStack)-1]
+
+	clear := output[len(output)-2]
+	encrypted := output[len(output)-1]
 
 	w, err := NewWrapper(publicPEM, privatePEM, []byte(passphrase))
 	if err != nil {
@@ -149,4 +101,83 @@ func TestCryptoPython(t *testing.T) {
 	if diff := deep.Equal(clear, string(decrypted)); diff != nil {
 		t.Fatal(diff)
 	}
+}
+
+// runPythonTest will use the set of test files to start a python process that will return
+// the console output any/or an error.  The caller can specify the max size of the buffer
+// used to hold the last (keepLines) of lines from the console.  If tmpDir is specified
+// that directory will be used to run the process and will not be removed after the
+// function completes, in the case it is blank then the function will generate a directory
+// run the python in it then remove it.
+//
+func runPythonTest(testFiles map[string]os.FileMode, tmpDir string, keepLines uint) (output []string, err kv.Error) {
+
+	output = []string{}
+
+	// I fthe optional temporary directory is not supplied then we create,
+	// use it and then remove it, this allows callers to load data into the
+	// directory they supply if they wish
+	if len(tmpDir) == 0 {
+		// Create a new TMPDIR because the python pip tends to leave dirt behind
+		// when doing pip builds etc
+		t, errGo := ioutil.TempDir("", "")
+		if errGo != nil {
+			return nil, kv.Wrap(errGo).With("stack", stack.Trace().TrimRuntime())
+		}
+		defer func() {
+			os.RemoveAll(t)
+		}()
+		tmpDir = t
+	}
+
+	expectedScript := ""
+
+	for fn, mode := range testFiles {
+		assetFN, errGo := filepath.Abs(fn)
+		if errGo != nil {
+			return nil, kv.Wrap(errGo).With("stack", stack.Trace().TrimRuntime())
+		}
+
+		destFN, errGo := filepath.Abs(filepath.Join(tmpDir, filepath.Base(assetFN)))
+		if errGo != nil {
+			return nil, kv.Wrap(errGo).With("stack", stack.Trace().TrimRuntime())
+		}
+
+		if _, err := CopyFile(assetFN, destFN); err != nil {
+			return nil, err
+		}
+
+		if errGo = os.Chmod(destFN, mode); errGo != nil {
+			return nil, kv.Wrap(errGo).With("destFN", destFN, "stack", stack.Trace().TrimRuntime())
+		}
+
+		if mode == 0700 {
+			expectedScript = destFN
+		}
+	}
+
+	// Save the output from the run using the last say 10 lines as a default otherwise
+	// use the callers specified number of lines if they specified any
+	if keepLines == 0 {
+		keepLines = 10
+	}
+	output = make([]string, keepLines, keepLines)
+
+	// Now setup is done execute the experiment
+	dataC := make(chan *string, 1)
+	go func() {
+		for {
+			select {
+			case line := <-dataC:
+				if line == nil {
+					return
+				}
+				// Push to the back of the stack of lines, then pop from the front
+				output = append(output, *line)
+				output = output[1:]
+			}
+		}
+	}()
+
+	return output, CmdRun(context.TODO(), expectedScript, dataC)
 }
