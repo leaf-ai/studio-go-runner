c6f13b12323e32ff8a3ac71a95785675d6d03157
Andrei Denissov <andrei.denissov@cognizant.com>
2021-08-09 18:36:31 -0700

    First stab at local file queues.

---
+++ internal/runner/localfilequeue_test.go
@@ -0,0 +1,13 @@
+// Copyright 2018-2021 (c) Cognizant Digital Business, Evolutionary AI. All rights reserved. Issued under the Apache 2.0 License.
+
+package runner
+
+// Unit tests for FileQueue implementation.
+
+import (
+	"testing"
+)
+
+func TestFileQueue(t *testing.T) {
+
+}
