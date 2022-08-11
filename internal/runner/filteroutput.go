695902ff6266e8a47c4f08da2cc780a3d6553921
Andrei Denissov <811862@cognizant.com>
2022-08-11 11:53:50 -0700

    Fixed regexpr machinery. Two-stages search/replace.

--- internal/runner/filteroutput.go
+++ internal/runner/filteroutput.go
@@ -2,37 +2,52 @@
 
 package runner
 
-import "regexp"
+import (
+	"github.com/andreidenissov-cog/go-service/pkg/log"
+	"regexp"
+)
 
 type OutputFilter interface {
 	Filter(input []byte) []byte
 }
 
 var (
-	logFilterExpr    = "git+https://(.*)@github.com"
-	logFilterReplace = []byte("git+https://****************@github.com")
+	logFilterExpr1    = "(git\\+https:|git:)//(.*?)@github.com"
+	logFilterExpr2    = "(\\w*?)(CREDENTIAL|PASSWORD|PASSWD|TOKEN|SECRET)(\\w*?)=(.*)$"
+	logFilterReplace1 = []byte("${1}//****************@github.com")
+	logFilterReplace2 = []byte("${1}${2}${3}=****************")
 )
 
 type LogFilterer struct {
-	expr *regexp.Regexp
+	expr1 *regexp.Regexp
+	expr2 *regexp.Regexp
 }
 
 func (lf *LogFilterer) Filter(input []byte) []byte {
-	if lf.expr != nil {
-		return lf.expr.ReplaceAll(input, logFilterReplace)
+	if lf.expr1 != nil {
+		input = lf.expr1.ReplaceAll(input, logFilterReplace1)
+	}
+	if lf.expr2 != nil {
+		input = lf.expr2.ReplaceAll(input, logFilterReplace2)
 	}
 	return input
 }
 
-func GetLogFilterer() OutputFilter {
-	filter := &LogFilterer{}
-
+func compileExpr(expr string, logger *log.Logger) (result *regexp.Regexp) {
 	defer func() {
 		if r := recover(); r != nil {
-			filter.expr = nil
+			logger.Error("FAILED to compile regexpr!", "regexpr:", expr, "reason:", r)
+			result = nil
 		}
 	}()
 
-	filter.expr = regexp.MustCompile(logFilterExpr)
+	result = regexp.MustCompile(expr)
+	return result
+}
+
+func GetLogFilterer(logger *log.Logger) OutputFilter {
+	filter := &LogFilterer{}
+	filter.expr1 = compileExpr(logFilterExpr1, logger)
+	filter.expr2 = compileExpr(logFilterExpr2, logger)
 	return filter
 }
