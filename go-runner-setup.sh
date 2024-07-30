3fd36648d1362575055a9633adea3992b25ca04c
Andrei Denissov <andrei.denissov@cognizant.com>
2024-07-30 15:37:31 -0700

    Fixes to build scripts.

--- go-runner-setup.sh
+++ go-runner-setup.sh
@@ -13,6 +13,8 @@
 # Optional:
 #   APP_HOME -          Where the go-runner source is git cloned into.
 #                       If not set we assume you are at the top-level unileaf directory
+#   GIT_BRANCH -        Current git branch for code being built
+#   GIT_COMMIT -        Current git commit id for code being built
 
 set -o errexit
 set -o errtrace
@@ -26,9 +28,23 @@ then
     cd "${APP_HOME}"
 fi
 
+if [ -z "${GIT_BRANCH}" ]
+then
+    export GIT_BRANCH="unknown branch"
+fi
+if [ -z "${GIT_COMMIT}" ]
+then
+    export GIT_COMMIT="unknown commit"
+fi
+
+source generate_git_info.sh "${GIT_BRANCH}" "${GIT_COMMIT}"
+
 export COMPONENT_NAME=runner-linux-amd64-cpu
 
 go clean -i
+go clean -modcache
+go clean -cache
+go clean -testcache
 go mod tidy
 go build -ldflags="-extldflags=-static" -buildvcs=false -tags="NO_CUDA" -o cmd/runner/bin/"${COMPONENT_NAME}"  ./cmd/runner
 
