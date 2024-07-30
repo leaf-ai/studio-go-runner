95620c641aff0be746d0e224e260bf6fa7db438b
Andrei Denissov <andrei.denissov@cognizant.com>
2024-07-29 19:30:19 -0700

    Added second Dockerfile stage.

--- go-runner-setup.sh
+++ go-runner-setup.sh
@@ -1,31 +1,18 @@
 #!/bin/bash
 
 # Usage:
-# cmd/unileaf-go-setup.sh [mdserver|mdserver-gateway]
+# ./go-runner-setup.sh
 #
 # This script is meant to setup the go environment and
-# build the mdserver and/or the mdserver-gateway.
+# build the go-runner.
 #
 # Additional parameters supplied as env var are needed as well.
 # Required:
 #   GOROOT - where your golang tool root lives
-#   GOPATH - the root for your golang source (see the cmd/mdserver.adoc for deets)
 #
 # Optional:
-#   APP_HOME -          Where the unileaf source is git cloned into.
+#   APP_HOME -          Where the go-runner source is git cloned into.
 #                       If not set we assume you are at the top-level unileaf directory
-#   COMPONENT_NAME -    The target of the build
-#                       If not set, we default to building mdserver only.
-#                       Will be overridden by the first command line argument.
-#   PROTOBUF_VERSION -  The version of the protoc compiler to use.
-#                       If not set, we default to a reasonable version used in builds.
-#   PROTOBUF_ZIP -      The zip file containing the protoc compiler to use.
-#                       If not set, we default to a reasonable version used in builds.
-#   PROTOBUF_URL -      The url where to find the zip file containing the protoc compiler to use.
-#                       If not set, we default to a reasonable version used in builds.
-#
-# See build_scripts/go_codefresh.yml and cmd/Dockerfile for
-# example of usage within our CI/CD system.
 
 set -o errexit
 set -o errtrace
@@ -39,10 +26,10 @@ then
     cd "${APP_HOME}"
 fi
 
-export COMPONENT=runner-linux-amd64-cpu
+export COMPONENT_NAME=runner-linux-amd64-cpu
 
 go clean -i
 go mod tidy
-go build -ldflags="-extldflags=-static" -tags="NO_CUDA" -o cmd/runner/bin/"${COMPONENT_NAME}"  ./cmd/runner
+go build -ldflags="-extldflags=-static" -buildvcs=false -tags="NO_CUDA" -o cmd/runner/bin/"${COMPONENT_NAME}"  ./cmd/runner
 
 echo "ALL DONE building ${COMPONENT_NAME}"
